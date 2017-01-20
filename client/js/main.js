a5c0dd73773f2dc21b470351c68530c371eee835
Maximilian Geithe <maximilian.geithe@ageto.net>
2017-01-20 09:32:59 +0100

    tried to implement shooting with p2 physics

--- client/js/main.js
+++ client/js/main.js
@@ -1,12 +1,16 @@
 var game = new Phaser.Game(640,360,Phaser.AUTO);
 
-var player, cursors;
+var player, cursors, bullets;
+
+var fireRate = 200;
+var nextFire = 0;
 
 var GameState = {
 	preload: function(){
 		game.load.image('background','assets/image/Background.png');
 		game.load.image('character','assets/image/Character.png');
 		game.load.image('stone','assets/image/Stone.png');
+		game.load.image('bullet','assets/image/Bullet.png');
 	},
 	create: function(){
 		game.add.tileSprite(0,0,2048,2048,'background');
@@ -27,6 +31,15 @@ var GameState = {
 		stone.body.static = true;
 		//stone.body.debug = true;
 
+		bullets = game.add.group();
+		bullets.enableBody = true;
+		bullets.physicsBodyType = Phaser.Physics.ARCADE;
+
+		bullets.createMultiple(50,'bullet');
+		bullets.setAll('checkWouldBounds',true);
+		bullets.setAll('outOfBoundsKill',true);
+
+
 		//game.camera.deadzone = new Phaser.Rectangle(100,100,440,160);
 
 	},
@@ -46,8 +59,54 @@ var GameState = {
 		else if(cursors.right.isDown) {
 			player.body.moveRight(150);
 		}
+
+		if(game.input.activePointer.isDown) {
+			fire();
+		}
 	}
 };
 
 game.state.add('GameState',GameState);
 game.state.start('GameState');
+
+function fire() {
+	if(game.time.now > nextFire && bullets.countDead() > 0) {
+		nextFire = game.time.now + fireRate;
+
+		var bullet = bullets.getFirstDead();
+
+		bullet.reset(player.x - 32, player.y - 32);
+
+		game.physics.p2.moveToPointer = function(displayObject,speed,pointer,maxTime) {
+			if(speed === undefined) speed = 60;
+			pointer = pointer || game.input.activePointer;
+			if(maxTime === undefined) maxTime = 0;
+
+			// @TODO
+		};		
+
+		game.physics.p2.angleToPointer = function(displayObject, pointer) {
+			if(pointer === undefined) pointer = game.input.activePointer;
+			if(world === undefined) world = false;
+
+			if(world) {
+				return Math.atan2(pointer.worldY - displayObject.world.y,pointer.worldX - displayObject.world.x);
+			}
+			else {
+				return Math.atan2(pointer.worldY - displayObject.y, pointer.worldX - displayObject.x);
+			}
+		};
+
+		game.physics.p2.distanceToPointer = function(displayObject, pointer){
+			if (pointer === undefined) pointer = game.input.activePointer;
+			if (world === undefined) world = false; 
+
+			var dx = (world) ? displayObject.world.x - pointer.worldX : displayObject.x - pointer.worldX;
+			var dy = (world) ? displayObject.world.y - pointer.worldY : displayObject.y - pointer.worldY;
+
+			return Math.sqrt(dx * dx + dy * dy);
+		};
+
+		game.physics.p2.moveToPointer(bullet, 300);
+	}
+};
